03489a42f24279c6183e406055517348a660055f
Lucy M Golcher <luciamongeg@gmail.com>
2019-11-10 23:42:15 -0600

    Avance del taller

--- TallerImperativo.c
+++ TallerImperativo.c
@@ -76,7 +76,7 @@ int strcmp(char *s, char *p){
     else {
         printf("0");
     }
-}/*
+}/
 //Escriba la siguiente función:
 //Void strcat(char *dest, char *src)
 //La función pega al final del string dest el valor del string src.
@@ -87,96 +87,7 @@ void _strcat(char *dest, char *src){
     
     printf("La nueva cadena es --> %c\n", aux);
 }
-/*
 
-//Ejercicio 2.7 Concatenar str
-void _strcat(char *dest, char *src){
-
-    char *auxres = (char*) malloc(strlen(dest) + strlen(src) + 1);
-    strcpy(auxres,dest);
-    strcpy(auxres, src);
-
-    printf("La nueva cadena es --> %c\n", auxres);
-}
-
-
-//Ejercicio 2.9 Frecuencia de las palabras
-struct detail
-{
-    char word[20];
-    int freq;
-};
-
-int update(struct detail [], const char [], int);
-
-int update(struct detail s[], const char unit[], int count)
-{
-    int i;
-
-    for (i = 0; i < count; i++)
-    {
-        if (strcmp(s[i].word, unit) == 0)
-        {
-            s[i].freq++;
-
-            return count;
-        }
-    }
-    strcpy(s[count].word, unit);
-    s[count].freq++;
-
-    return (count + 1);
-}
-
-int frecuencia()
-{
-    struct detail s[10];
-    char string[100], unit[20], c;
-    int i = 0, freq = 0, j = 0, count = 0, num = 0;
-
-    for (i = 0; i < 10; i++)
-    {
-        s[i].freq = 0;
-    }
-    printf("Enter string: ");
-    i = 0;
-    do
-    {
-        fflush(stdin);
-        c = getchar();
-        string[i++] = c;
-
-    } while (c != '\n');
-    string[i - 1] = '\0';
-    printf("The string entered is: %s\n", string);
-    for (i = 0; i < strlen(string); i++)
-    {
-        while (i < strlen(string) && string[i] != ' ' && isalnum(string[i]))
-        {
-            unit[j++] = string[i++];
-        }
-        if (j != 0)
-        {
-            unit[j] = '\0';
-            count = update(s, unit, count);
-            j = 0;
-        }
-    }
-
-    printf("*****************\nWord\tFrequency\n*****************\n");
-    for (i = 0; i < count; i++)
-    {
-        printf("%s\t   %d\n", s[i].word, s[i].freq);
-        if (s[i].freq > 1)
-        {
-            num++;
-        }
-    }
-    printf("The number of repeated words are %d.\n", num);
-
-    return 0;
-}
-*/
 
 int main(){
     //equivalenteMillas();
